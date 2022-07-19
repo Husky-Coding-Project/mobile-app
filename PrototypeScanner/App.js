6a21e6601f20b7460e039a2f1e361e80c557c2e6
root <root@Util.localdomain>
2022-07-18 23:25:25 -0700

    actually add button code

--- PrototypeScanner/App.js
+++ PrototypeScanner/App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [ count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Clicked {count} times</Text>
+      <Button 
+        onPress={() => setCount(count + 1)}
+        title="Generate QR code"
+      />
+      <Text>Clicked {count} times</Text>
+      <Button
+        onPress={() => setCount(count + 1)}
+        title="Scan QR code"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'aliceblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
